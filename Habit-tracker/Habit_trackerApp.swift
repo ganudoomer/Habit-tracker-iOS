c2ab0501e4210425982d44e7169d3fec11cee4ef
ganudoomer <ganuiscool@gmail.com>
2021-10-16 21:44:38 +0530

    Initial Commit

---
+++ Habit-tracker/Habit_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_trackerApp.swift
+//  Habit-tracker
+//
+//  Created by Sree on 16/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Habit_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
